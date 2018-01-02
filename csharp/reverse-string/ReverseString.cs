13c4b99f2d6910a6ab2de022a52987143ea97f05
sslampa <sslampa@gmail.com>
2018-01-01 21:49:02 -0800

    Solve reverse-string

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string reversed = "";
+        for (int i = input.Length - 1; i >= 0; i--) {
+            reversed += input[i];
+        }
+        return reversed;
+    }
+}
