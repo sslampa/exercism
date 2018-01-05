8d635bef7c7a92ac4a8a0a29526a1be817d45022
sslampa <sslampa@gmail.com>
2018-01-04 19:57:49 -0800

    Solve SumOfMultiples - C#

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> group = new List<int>();
+        foreach (int m in multiples)
+        {
+            int current = m;
+            int i = 1;
+            while (current < max)
+            {
+                current = m * i++;
+                if (current < max && group.Contains(current) == false)
+                {
+                    group.Add(current);
+                }
+            }
+        }
+
+        return group.Sum();
     }
 }
