e580f5791ae0a57220899ad79e21eb1628931713
sslampa <sslampa@gmail.com>
2018-01-05 22:14:58 -0800

    Solve SecretHandshake

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -15,6 +15,32 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    bin = Integer.to_string(code, 2) |> String.reverse()
+    String.split(bin, "")
+    |> Enum.filter(fn (x) -> x != "" end)
+    |> Enum.with_index(0)
+    |> Enum.map(fn (x) -> 
+      b = (elem(x, 0) === "1")
+      power = elem(x, 1)
+      m = :math.pow(2, power)
+      cond do
+        m == 1 && b -> "wink"
+        m == 2 && b -> "double blink"
+        m == 4 && b -> "close your eyes"
+        m == 8 && b -> "jump"
+        m == 16 && b -> "reverse"
+        true -> ""
+      end
+    end)
+    |> (fn(x) ->
+      [head | tail] = Enum.reverse(x)
+      if head === "reverse" do
+        tail
+      else
+        x
+      end
+    end).()
+    |> Enum.filter(fn(i) -> i != "" end)
   end
 end
 
