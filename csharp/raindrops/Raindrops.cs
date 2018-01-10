2b5cbef34acb390c0daf69b99538b1c3857bf85c
sslampa <sslampa@gmail.com>
2018-01-09 22:40:39 -0800

    Solve raindrops

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -4,6 +4,31 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string result = "";
+        for (int i = 1; i <= number; i++)
+        {
+            if (number % i == 0) 
+            {
+                switch (i)
+                {
+                    case 3:
+                        result += "Pling";
+                        break;
+                    case 5:
+                        result += "Plang";
+                        break;
+                    case 7:
+                        result += "Plong";
+                        break;
+                }
+            }
+        }
+
+        if (result == "")
+        {
+            return number.ToString();
+        }
+
+        return result;
     }
 }
