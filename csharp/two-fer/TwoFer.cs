3408f299453fd709d4694dad05b1fd7f0d0aa61a
sslampa <sslampa@gmail.com>
2018-01-10 23:11:19 -0800

    Solve twofer

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      public static string Name(string input = null)
+      {
+          string name = (input == null) ? "you" : input;
+
+          return string.Format("One for {0}, one for me.", name);
+      }
+  }
