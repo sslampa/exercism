054b07df90de896599dacf27de2a59ae94b70374
sslampa <sslampa@gmail.com>
2018-01-13 18:36:40 -0800

    Solve collatzconjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        int count = 0;
+
+        if (number <= 0)
+        {
+            throw new ArgumentException();
+        }
+
+        while (number != 1)
+        {
+            number = (number % 2 == 0) ? number / 2 : 3 * number + 1;
+            count++;
+        }
+
+        return count;
+    }
+}
