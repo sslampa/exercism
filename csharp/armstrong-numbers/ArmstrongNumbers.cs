5b2d676362fd57d596a8f741e72ae17bd9217a35
sslampa <sslampa@gmail.com>
2018-01-13 19:08:26 -0800

    Solve ArmstrongNumbers

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string n = number.ToString();
+        double length = Convert.ToDouble(n.Length);
+        int total = 0;
+
+        foreach (char c in n)
+        {
+            double value = Convert.ToDouble(c.ToString());
+            total += Convert.ToInt32(Math.Pow(value, length));
+        }
+
+        return total == number;
+    }
+}
