f85f908f19f5cd1022166c8bea5a446af8144bcb
sslampa <sslampa@gmail.com>
2018-01-13 17:47:16 -0800

    Solve accumulate

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -5,6 +5,10 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        
+        foreach(var c in collection)
+        {
+            var tmp = func(c);
+            yield return tmp;
+        }
     }
 }
