d0b103c0fe3e57077f49572aac84ee9c30fd2ecd
sslampa <sslampa@gmail.com>
2018-01-15 22:13:31 -0800

    Solve KindergartenGarden

--- csharp/kindergarten-garden/KindergartenGarden.cs
+++ csharp/kindergarten-garden/KindergartenGarden.cs
@@ -11,8 +11,14 @@ public enum Plant
 
 public class KindergartenGarden
 {    
+    private string _top;
+    private string _bottom;
+    private string _students = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public KindergartenGarden(string diagram)
     {
+        string[] rows = diagram.Split('\n');
+        _top = rows[0];
+        _bottom = rows[1];
     }
 
     public KindergartenGarden(string diagram, IEnumerable<string> students)
@@ -21,6 +27,29 @@ public class KindergartenGarden
 
     public IEnumerable<Plant> Plants(string student)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char firstLetter = student[0];
+        int index = _students.IndexOf(firstLetter);
+        int start = 2 * index;
+
+        char[] plants = {_top[start], _top[start + 1], _bottom[start], _bottom[start + 1]};
+        foreach (char p in plants)
+        {
+            yield return convertToEnum(p);
+        }
+    }
+
+    private Plant convertToEnum(char p) 
+    {
+        switch (p)
+        {
+            case 'V':
+                return Plant.Violets;
+            case 'R':
+                return Plant.Radishes;
+            case 'C':
+                return Plant.Clover;
+            default:
+                return Plant.Grass;
+        }
     }
 }
